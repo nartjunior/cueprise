f7ab909a854e64711877ca3f6b3eababd4ba93ad
Nart Pacarizi <nart.pacarizi@gmail.com>
2023-09-18 11:13:14 +0200

    Fixed Meme Screen, added persistence to Sign Up Screen and created Profile Screen

---
+++ lib/number_text_input_formatter.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/services.dart';
+
+class NumberTextInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    final int newTextLength = newValue.text.length;
+    int selectionIndex = newValue.selection.end;
+    int usedSubstringIndex = 0;
+    final StringBuffer newText = StringBuffer();
+    if (newTextLength >= 1) {
+      newText.write('+');
+      if (newValue.selection.end >= 1) selectionIndex++;
+    }
+    if (newTextLength >= 3) {
+      newText.write('${newValue.text.substring(0, usedSubstringIndex = 2)} ');
+      if (newValue.selection.end >= 2) selectionIndex += 1;
+    }
+    // Dump the rest.
+    if (newTextLength >= usedSubstringIndex) {
+      newText.write(newValue.text.substring(usedSubstringIndex));
+    }
+    return TextEditingValue(
+      text: newText.toString(),
+      selection: TextSelection.collapsed(offset: selectionIndex),
+    );
+  }
+}
